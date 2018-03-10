9a9cfbc1c35c09490ac69424edb4bf9d7dd1b813
johnsonvu <johnsonvu96@gmail.com>
2018-03-10 11:43:59 -0800

    Replace commands in header with larger set

--- modules/uas_collision_avoidance/collision_avoidance.hpp
+++ modules/uas_collision_avoidance/collision_avoidance.hpp
@@ -7,9 +7,8 @@
 class CollisionAvoidance
 {
 public:
-    enum class MissionPlannerCommands : uint8_t
+    enum class MissionPlannerCommand : uint8_t
     {
-        // Mission Planner Commands
         WAYPOINT = 16,
         SPLINE_WAYPOINT = 82,
         LOITER_TURNS = 18,
@@ -19,9 +18,26 @@ public:
         LAND = 21,
         TAKEOFF = 22,
         DELAY = 93,
-        GUIDED_ENABLE = 92,
+        GUIDED_ENABLED = 92,
         PAYLOAD_PLACE = 94,
-        DO_CHANGE_SPEED = 178
+        DO_GUIDED_LIMITS = 222,
+        DO_SET_ROI = 201,
+        CONDITION_DELAY = 112,
+        CONDITION_CHANGE_ALT = 113,
+        CONDITION_DISTANCE = 114,
+        CONDITION_YAW = 115,
+        DO_JUMP = 177,
+        DO_CHANGE_SPEED = 178,
+        DO_GRIPPER = 211,
+        DO_PARACHUTE = 208,
+        DO_SET_CAM_TRIGG_DIST = 206,
+        DO_SET_RELAY = 181,
+        DO_REPEAT_RELAY = 182,
+        DO_SET_SERVO = 183,
+        DO_REPEAT_SERVO = 184,
+        DO_DIGICAM_CONFIGURE = 202,
+        DO_DIGICAM_CONTROL = 203,
+        DO_MOUNT_CONTROL = 205
     };
 
     CollisionAvoidance();
