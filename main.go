e60996675a08fa866b8f73ecad484ddee2203f4f
Aden Chan <adenchan@gmail.com>
2022-10-08 16:03:08 -0700

    Initial Commit - Created qr.go and added -qr argument to specifiy SkyScanner path
    GET /qr/start - Start SkyScanner
    POST /qr/return - Endpoint for SkyScanner to return to

--- main.go
+++ main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"flag"
 	"github.com/labstack/echo/v4"
 )
 
+var SkyScannerPath *string
+
 func main() {
+	SkyScannerPath = flag.String("qr", "", "Absolute Path to SkyScanner qr_code.py")
+	flag.Parse()
+
 	e := echo.New()
 
 	e.GET("/", Hello)
+	e.GET("qr/start", InitializeQR)
+	e.POST("qr/return", ParseQRData)
 
 	e.Logger.Fatal(e.Start(":1323"))
 }
+
