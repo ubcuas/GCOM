3ba243621ee475b8020a9ffa6ca7a9895aa04d95
Aden Chan <adenchan@gmail.com>
2022-11-05 12:04:37 -0700

    This reverts commit a4c406c67341ac9f301fc054bcba077196860ba4, reversing changes made to 8be1b8907d3cec5b6844a78270964bdabda4e4ae. revert merge with 5-skyscanner-integration

--- main.go
+++ main.go
@@ -1,28 +1,13 @@
 package main
 
 import (
-	"log"
-	"os"
-
-	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 )
 
-var SkyScannerPath string
-
 func main() {
-	err := godotenv.Load(".env")
-  	if err != nil {
-    	log.Fatal("Error loading .env file")
-  	}
-	SkyScannerPath = os.Getenv("PATH_TO_SKYSCANNER_FOLDER") + "\\src\\qr_code.py"
-
 	e := echo.New()
 
 	e.GET("/", Hello)
-	e.GET("qr/start", InitializeQR)
-	e.POST("qr/return", ParseQRData)
 
 	e.Logger.Fatal(e.Start(":1323"))
 }
-
