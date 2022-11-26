9f048cb54bb9e8d97afca3eb5bc125c54b49d7d3
kenndu <59485248+kenndu@users.noreply.github.com>
2022-11-26 15:03:23 -0800

    add docs

--- mp.go
+++ mp.go
@@ -5,25 +5,35 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"strings"
 )
 
-// return a pointer to a Queue struct formed from getting the current Queue of Waypoints from MP
-// func GetQueue() (string, error) {
+/**
+ * Gets the current route ("Queue" of Waypoints) that constitute the path the drone is folllowing, from Mission Planner
+ *
+ * Returns: - Pointer to a Queue struct containing Waypoints generated from the Waypoint data
+ * 				given in the response from Mission Planner
+ *			- Error if anything goes wrong
+ */
 func GetQueue() (*Queue, error) {
 	var endpoint = getEnvVariable("MP_ROUTE") + "/queue"
 
 	req, err := http.NewRequest("GET", endpoint, nil)
 	req.Header.Set("Content-Type", "application-json")
 	if err != nil {
-		panic(err)
+		// panic(err)
+		log.Fatal(err)
+		return nil, err
 	}
 
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
+		return nil, err
+		// panic(err)
 	}
 	defer resp.Body.Close()
 
@@ -40,31 +50,45 @@ func GetQueue() (*Queue, error) {
 
 	err = json.Unmarshal(body, &queue)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
+		return nil, err
+		// panic(err)
 	}
 
 	return &Queue{Queue: queue}, nil
 }
 
+/**
+ * Overrides the current route ("Queue" of Waypoints) that the drone is following with a new Queue
+ *
+ * Param: queue - a pointer to a Queue struct containing the new sequence of Waypoints to follow
+ *
+ * Returns: - Error if anything goes wrong
+ */
 // take a pointer to a Queue struct and post those waypoints to MP
 func PostQueue(queue *Queue) error {
 	var endpoint = getEnvVariable("MP_ROUTE") + "/queue"
 
 	json, err := json.Marshal(queue.Queue)
 	if err != nil {
+		log.Fatal(err)
 		return err
 	}
 
 	req, err := http.NewRequest("POST", endpoint, strings.NewReader(string(json)))
 	req.Header.Set("Content-Type", "application-json")
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
+		return err
+		// panic(err)
 	}
 
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
+		return err
+		// panic(err)
 	}
 	defer resp.Body.Close()
 
@@ -77,20 +101,31 @@ func PostQueue(queue *Queue) error {
 	return nil
 }
 
-// returns a pointer to an AircraftStatus struct with the current AircraftStatus from MP
+//TODO: Maybe follow singleton design pattern for AircraftStatus? -> make this method callable only on an instance
+//		of AircraftStatus struct
+/**
+ * Gets the current aircraft status from Mission Planner
+ *
+ * Returns: - Pointer to an AircraftStatus struct with field info pulled from Mission Planner
+ *			- Error if anything goes wrong
+ */
 func GetAircraftStatus() (*AircraftStatus, error) {
 	var endpoint = getEnvVariable("MP_ROUTE") + "/status"
 
 	req, err := http.NewRequest("GET", endpoint, nil)
 	req.Header.Set("Content-Type", "application-json")
 	if err != nil {
-		panic(err)
+		// panic(err)
+		log.Fatal(err)
+		return nil, err
 	}
 
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		panic(err)
+		// panic(err)
+		log.Fatal(err)
+		return nil, err
 	}
 	defer resp.Body.Close()
 
@@ -107,7 +142,9 @@ func GetAircraftStatus() (*AircraftStatus, error) {
 
 	err = json.Unmarshal(body, &stat)
 	if err != nil {
-		panic(err)
+		// panic(err)
+		log.Fatal(err)
+		return nil, err
 	}
 
 	return &stat, nil
