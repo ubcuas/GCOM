600673b8eea3b0dba286686203094bef6814b944
kenndu <59485248+kenndu@users.noreply.github.com>
2022-12-03 13:31:27 -0800

    add endpoints to load waypoints into GCOM

--- controllers.go
+++ controllers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -13,13 +14,46 @@ func Hello(c echo.Context) error {
 // endpoint we serve that responds with a list of all the waypoints currently in the database
 func GetWaypoints(c echo.Context) error {
 
-	return nil
+	queue, err := getAllWaypoints()
+	allWaypoints := queue.Queue
+	if err != nil {
+		// log.Fatal(err)
+		Error.Println(err)
+		return err
+	}
+
+	return c.JSON(http.StatusOK, allWaypoints)
+
 }
 
 // endpoint we serve that takes a JSON list of Waypoints and registers them all in the database
 func PostWaypoints(c echo.Context) error {
 
-	return nil
+	var waypoints []Waypoint
+
+	err := c.Bind(&waypoints)
+	if err != nil {
+		// log.Fatal(err)
+		Error.Println(err)
+		return err
+	}
+
+	// waypoints[0].Create()
+	// fmt.Println(waypoints)
+
+	fmt.Println("Registering waypoints...")
+	for _, wp := range waypoints {
+		err = wp.Create()
+		if err != nil {
+			// log.Fatal(err)
+			Error.Println(err)
+			return err
+		}
+		fmt.Println(wp)
+	}
+	Info.Println("Registered waypoints: ", waypoints, "to the database")
+	return c.String(http.StatusOK, "Waypoints successfully registered!")
+	// return c.JSON(http.StatusOK, waypoints)
 }
 
 // endpoint we serve that responds with a list of all the routes currently in the database
