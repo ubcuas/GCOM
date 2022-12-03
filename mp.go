0c62ff46a2dc982343a0f66f76ea104fd532a49d
kenndu <59485248+kenndu@users.noreply.github.com>
2022-12-03 14:55:25 -0800

    add wrappers for land/sethome/rtl endpoints

--- mp.go
+++ mp.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -155,7 +156,7 @@ func GetAircraftStatus() (*AircraftStatus, error) {
  *
  * Returns: - Error if anything goes wrong (ex. if the aircraft is already locked)
  */
-func lockAircraft() error {
+func LockAircraft() error {
 	var endpoint = getEnvVariable("MP_ROUTE") + "/lock"
 
 	req, err := http.NewRequest("GET", endpoint, nil)
@@ -190,7 +191,7 @@ func lockAircraft() error {
  *
  * Returns: - Error if anything goes wrong (ex. if the aircraft is already unlocked)
  */
-func unlockAircraft() error {
+func UnlockAircraft() error {
 	var endpoint = getEnvVariable("MP_ROUTE") + "/unlock"
 
 	req, err := http.NewRequest("GET", endpoint, nil)
@@ -215,6 +216,107 @@ func unlockAircraft() error {
 	if resp.Status != "200 OK" {
 		Error.Println("Aircraft already unlocked: " + resp.Status)
 		return errors.New("Aircraft already unlocked: " + resp.Status)
+	}
+
+	return nil
+}
+
+/**
+ * Send the aircraft back to the original launch site
+ *
+ * Returns: - Error if anything goes wrong
+ */
+func ReturnToLaunch() error {
+	var endpoint = getEnvVariable("MP_ROUTE") + "/rtl"
+
+	req, err := http.NewRequest("GET", endpoint, nil)
+	req.Header.Set("Content-Type", "application-json")
+	if err != nil {
+		// panic(err)
+		// log.Fatal(err)
+		Error.Println(err)
+		return err
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		Error.Println(err)
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.Status != "200 OK" {
+		Error.Println("Failed to return to launch: " + resp.Status)
+		return errors.New("Failed to return to launch: " + resp.Status)
+	}
+
+	return nil
+}
+
+/**
+ * Immediately descend the aircraft and land over current position
+ *
+ * Returns: - Error if anything goes wrong
+ */
+func LandImmediately() error {
+	var endpoint = getEnvVariable("MP_ROUTE") + "/land"
+
+	req, err := http.NewRequest("GET", endpoint, nil)
+	req.Header.Set("Content-Type", "application-json")
+	if err != nil {
+		Error.Println(err)
+		return err
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		Error.Println(err)
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.Status != "200 OK" {
+		Error.Println("Failed to land " + resp.Status)
+		return errors.New("Failed to land" + resp.Status)
+	}
+
+	return nil
+}
+
+/**
+ * Sets the 'home' waypoint of the aircraft
+ *
+ * Returns: - Error if anything goes wrong
+ */
+func PostHome(home *Waypoint) error {
+	var endpoint = getEnvVariable("MP_ROUTE") + "/home"
+
+	reqBody, err := json.Marshal(home)
+	if err != nil {
+		Error.Println(err)
+		return err
+	}
+
+	req, err := http.NewRequest("POST", endpoint, bytes.NewBuffer(reqBody))
+	req.Header.Set("Content-Type", "application-json")
+	if err != nil {
+		Error.Println(err)
+		return err
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		Error.Println(err)
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.Status != "200 OK" {
+		Error.Println("Failed to set home: " + resp.Status)
+		return errors.New("Failed to set home: " + resp.Status)
 	}
 
 	return nil
