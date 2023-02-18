a5ccc23f8d1065ce0ae8508fe1f3b8a8e8b652fd
kenndu <59485248+kenndu@users.noreply.github.com>
2023-02-18 13:15:34 -0800

    finish tests for current controllers

--- controllers_test.go
+++ controllers_test.go
@@ -11,19 +11,19 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestHello(t *testing.T) {
-	e := echo.New()
+// func TestHello(t *testing.T) {
+// 	e := echo.New()
 
-	req := httptest.NewRequest(http.MethodGet, "/", nil)
-	rec := httptest.NewRecorder()
+// 	req := httptest.NewRequest(http.MethodGet, "/", nil)
+// 	rec := httptest.NewRecorder()
 
-	c := e.NewContext(req, rec)
+// 	c := e.NewContext(req, rec)
 
-	if assert.NoError(t, Hello(c)) {
-		assert.Equal(t, http.StatusOK, rec.Code)
-		assert.Equal(t, "Hello, World!", rec.Body.String())
-	}
-}
+// 	if assert.NoError(t, Hello(c)) {
+// 		assert.Equal(t, http.StatusOK, rec.Code)
+// 		assert.Equal(t, "Hello, World!", rec.Body.String())
+// 	}
+// }
 
 func TestPostWaypoints(t *testing.T) {
 	cleanDB()
@@ -173,6 +173,151 @@ func TestPostRoutes(t *testing.T) {
 	if assert.NoError(t, PostRoutes(c)) {
 		assert.Equal(t, http.StatusOK, rec.Code)
 		assert.Equal(t, "AEACRoutes registered!", rec.Body.String())
+	}
+
+	cleanDB()
+}
+
+func TestGetRoutes(t *testing.T) {
+	cleanDB()
+
+	e := echo.New()
+
+	r1 := AEACRoutes{
+		ID:               -1,
+		Number:           1,
+		StartWaypoint:    "alpha",
+		EndWaypoint:      "delta",
+		Passengers:       5,
+		MaxVehicleWeight: 30.5,
+		Value:            13.2,
+		Remarks:          "",
+		Order:            0,
+	}
+
+	r2 := AEACRoutes{
+		ID:               -1,
+		Number:           2,
+		StartWaypoint:    "delta",
+		EndWaypoint:      "zeta",
+		Passengers:       10,
+		MaxVehicleWeight: 20.05,
+		Value:            17.3,
+		Remarks:          "remark",
+		Order:            1,
+	}
+
+	rSlice := []AEACRoutes{r1, r2}
+
+	jsonBody, _ := json.Marshal(rSlice)
+
+	req := httptest.NewRequest(http.MethodPost, "/routes", strings.NewReader(string(jsonBody)))
+	req.Header.Set("Content-Type", "application/json")
+	rec := httptest.NewRecorder()
+
+	c := e.NewContext(req, rec)
+
+	if assert.NoError(t, PostRoutes(c)) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+		assert.Equal(t, "AEACRoutes registered!", rec.Body.String())
+
+		//check if get returns all the routes
+
+		req = httptest.NewRequest(http.MethodGet, "/routes", nil)
+		rec := httptest.NewRecorder()
+
+		c = e.NewContext(req, rec)
+
+		if assert.NoError(t, GetRoutes(c)) {
+			r1.ID = 1
+			r2.ID = 2
+			routeSlice := []AEACRoutes{r1, r2}
+
+			jsonBody, _ = json.Marshal(routeSlice)
+
+			// Info.Println(string(jsonBody))
+
+			assert.Equal(t, http.StatusOK, rec.Code)
+			assert.Equal(t, string(jsonBody)+"\n", rec.Body.String())
+		}
+	}
+
+	cleanDB()
+}
+
+func TestGetNextRoute(t *testing.T) {
+	cleanDB()
+
+	e := echo.New()
+
+	r1 := AEACRoutes{
+		ID:               -1,
+		Number:           1,
+		StartWaypoint:    "alpha",
+		EndWaypoint:      "delta",
+		Passengers:       5,
+		MaxVehicleWeight: 30.5,
+		Value:            13.2,
+		Remarks:          "",
+		Order:            5,
+	}
+
+	r2 := AEACRoutes{
+		ID:               -1,
+		Number:           2,
+		StartWaypoint:    "delta",
+		EndWaypoint:      "zeta",
+		Passengers:       10,
+		MaxVehicleWeight: 20.05,
+		Value:            17.3,
+		Remarks:          "remark",
+		Order:            3,
+	}
+
+	r3 := AEACRoutes{
+		ID:               -1,
+		Number:           3,
+		StartWaypoint:    "delta",
+		EndWaypoint:      "alpha",
+		Passengers:       10,
+		MaxVehicleWeight: 20.05,
+		Value:            17.3,
+		Remarks:          "next waypoint",
+		Order:            0,
+	}
+
+	rSlice := []AEACRoutes{r1, r2, r3}
+
+	jsonBody, _ := json.Marshal(rSlice)
+
+	req := httptest.NewRequest(http.MethodPost, "/routes", strings.NewReader(string(jsonBody)))
+	req.Header.Set("Content-Type", "application/json")
+	rec := httptest.NewRecorder()
+
+	c := e.NewContext(req, rec)
+
+	if assert.NoError(t, PostRoutes(c)) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+		assert.Equal(t, "AEACRoutes registered!", rec.Body.String())
+
+		//check that getNextRoute returns the route with the lowest order
+
+		req = httptest.NewRequest(http.MethodGet, "/nextroute", nil)
+		rec := httptest.NewRecorder()
+
+		c = e.NewContext(req, rec)
+
+		if assert.NoError(t, GetNextRoute(c)) {
+			r3.ID = 3
+			// routeSlice := []AEACRoutes{r1, r2}
+
+			jsonBody, _ = json.Marshal(r3)
+
+			// Info.Println(string(jsonBody))
+
+			assert.Equal(t, http.StatusOK, rec.Code)
+			assert.Equal(t, string(jsonBody)+"\n", rec.Body.String())
+		}
 	}
 
 	cleanDB()
