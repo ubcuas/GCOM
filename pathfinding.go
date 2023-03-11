e3ccc82512891ae6c8f7d26e4787dfb9a6c0b752
kenndu <59485248+kenndu@users.noreply.github.com>
2023-03-11 14:32:05 -0800

    add tests for creating input file

--- pathfinding.go
+++ pathfinding.go
@@ -1,14 +1,13 @@
 package main
 
-//helper functions to convert existing types to pathfinding types
-func (wp Waypoint) toPFWaypoint() PFWaypoint {
-	return PFWaypoint{
-		ID:        wp.ID,
-		Latitude:  wp.Latitude,
-		Longitude: wp.Longitude,
-	}
-}
+import (
+	"encoding/json"
+	"errors"
+	"os"
+	"os/exec"
+)
 
+// helper functions to convert existing types to pathfinding types
 func (r AEACRoutes) toPFRoute(startWaypointID int, endWaypointID int) PFRoute {
 	return PFRoute{
 		ID:          r.ID,
@@ -17,4 +16,78 @@ func (r AEACRoutes) toPFRoute(startWaypointID int, endWaypointID int) PFRoute {
 	}
 }
 
-//func to automatically send email with routes we're taking to competition
+/**
+ * Create the input "Text.json" file for the pathfinding binary and write it to disk
+ */
+func (pfInput PathfindingInput) createPathfindingInput() error {
+	file, err := json.MarshalIndent(pfInput, "", " ")
+	if err != nil {
+		Error.Println(err)
+		return err
+	}
+
+	_ = os.WriteFile("./pathfinding/Text.json", file, 0666)
+
+	return nil
+}
+
+/**
+ * Execute the pathfinding binary, creating an output file "Output.json" in the process
+ */
+func runPathfinding() (bool, error) {
+	if _, err := os.Stat("./pathfinding/Text.json"); errors.Is(err, os.ErrNotExist) {
+		return false, errors.New("Text.json does not exist")
+	}
+
+	binary := exec.Command("./pathfinding/UAS-Pathfinding.exe")
+	if errors.Is(binary.Err, exec.ErrDot) {
+		binary.Err = nil
+	}
+	if err := binary.Run(); err != nil {
+		Error.Println(err)
+		return false, err
+	}
+
+	if _, err := os.Stat("Output.json"); errors.Is(err, os.ErrNotExist) {
+		return false, errors.New("Output.json was not properly created")
+	}
+
+	return true, nil
+}
+
+/*
+*
+  - Read the output file "Output.json" and return the results in a slice of AEACRoutes,
+    using the route IDs from pfInput
+*/
+func (pfInput PathfindingInput) readPathfindingOutput() (*[]AEACRoutes, error) {
+	if _, err := os.Stat("./pathfinding/Output.json"); errors.Is(err, os.ErrNotExist) {
+		return nil, errors.New("Output.json does not exist")
+	}
+
+	file, err := os.ReadFile("./pathfinding/Output.json")
+	if err != nil {
+		Error.Println(err)
+		return nil, err
+	}
+
+	var routeIDs []int
+
+	err = json.Unmarshal(file, &routeIDs)
+	if err != nil {
+		Error.Println(err)
+		return nil, err
+	}
+
+	var routes []AEACRoutes
+	for index, routeID := range routeIDs {
+		for _, route := range pfInput.AEACRoutes {
+			if route.ID == routeID {
+				routes = append(routes, route)
+				routes[index].Order = index
+			}
+		}
+
+	}
+	return &routes, nil
+}
--- pathfinding_test.go
+++ pathfinding_test.go
@@ -6,21 +6,21 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestConvertWPtoPFWP(t *testing.T) {
-	wp := Waypoint{
-		ID:        1,
-		Name:      "gcomWP1",
-		Longitude: -6.2,
-		Latitude:  149.1651830,
-		Altitude:  20.0,
-	}
+// func TestConvertWPtoPFWP(t *testing.T) {
+// 	wp := Waypoint{
+// 		ID:        1,
+// 		Name:      "gcomWP1",
+// 		Longitude: -6.2,
+// 		Latitude:  149.1651830,
+// 		Altitude:  20.0,
+// 	}
 
-	pfwp := wp.toPFWaypoint()
+// 	pfwp := wp.toPFWaypoint()
 
-	assert.Equal(t, wp.ID, pfwp.ID)
-	assert.Equal(t, wp.Latitude, pfwp.Latitude)
-	assert.Equal(t, wp.Longitude, pfwp.Longitude)
-}
+// 	assert.Equal(t, wp.ID, pfwp.ID)
+// 	assert.Equal(t, wp.Latitude, pfwp.Latitude)
+// 	assert.Equal(t, wp.Longitude, pfwp.Longitude)
+// }
 
 func TestConvertAEACRoutestoPFRoute(t *testing.T) {
 	cleanDB()
@@ -61,4 +61,94 @@ func TestConvertAEACRoutestoPFRoute(t *testing.T) {
 		assert.Equal(t, wp1.ID, pfr.WaypointIDs[0])
 		assert.Equal(t, wp2.ID, pfr.WaypointIDs[1])
 	}
+
+	cleanDB()
+}
+
+func TestCreateInputJson(t *testing.T) {
+	cleanDB()
+
+	wp1 := Waypoint{
+		ID:        -1,
+		Name:      "alpha",
+		Longitude: -6.2,
+		Latitude:  149.1651830,
+		Altitude:  20.0,
+	}
+
+	wp2 := Waypoint{
+		ID:        -1,
+		Name:      "beta",
+		Longitude: -36.3637798,
+		Latitude:  147.1651830,
+		Altitude:  20.0,
+	}
+
+	wp3 := Waypoint{
+		ID:        -1,
+		Name:      "gamma",
+		Longitude: -38.3637798,
+		Latitude:  150.1651830,
+		Altitude:  50.0,
+	}
+
+	r1 := AEACRoutes{
+		ID:               -1,
+		Number:           2,
+		StartWaypoint:    "alpha",
+		EndWaypoint:      "beta",
+		Passengers:       10,
+		MaxVehicleWeight: 1000,
+		Value:            50.2,
+		Order:            0,
+	}
+
+	r2 := AEACRoutes{
+		ID:               -1,
+		Number:           3,
+		StartWaypoint:    "beta",
+		EndWaypoint:      "gamma",
+		Passengers:       10,
+		MaxVehicleWeight: 1000,
+		Value:            50.2,
+		Order:            1,
+	}
+
+	assert.NoError(t, wp1.Create())
+	assert.NoError(t, wp2.Create())
+	assert.NoError(t, wp3.Create())
+	assert.NoError(t, r1.Create())
+	assert.NoError(t, r2.Create())
+
+	aeacRoutes := []AEACRoutes{r1, r2}
+
+	routefinder := RouteFinder{
+		MaxFlyingDistance:  40.6,
+		StartingWaypointID: 2,
+	}
+
+	rerouter := ReRouter{
+		CurrentLong:         50.4,
+		CurrentLat:          60.8,
+		WaypointObstacleIDs: []int{1, 5, 2},
+		ReRouteWaypointID:   3,
+	}
+
+	r1pf := r1.toPFRoute(1, 2)
+	r2pf := r2.toPFRoute(2, 3)
+
+	pfInput := PathfindingInput{
+		NumWaypoints: 3,
+		NumRoutes:    2,
+		WPQueue:      []Waypoint{wp1, wp2, wp3},
+		RouteQueue:   []PFRoute{r1pf, r2pf},
+		RouteFinder:  routefinder,
+		ReRouter:     rerouter,
+		AEACRoutes:   aeacRoutes,
+	}
+
+	err := pfInput.createPathfindingInput()
+	assert.NoError(t, err)
+
+	cleanDB()
 }
